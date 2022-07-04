00fb2342630f7867220e08d4a837643fdac62b58
hello30190f <108202763+hello30190f@users.noreply.github.com>
2022-07-04 12:45:23 +0900

    Use class

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
 import tkinter
 
+class graphic():
+
+    def __init__(self) -> None:
+        self.picture = tkinter.Canvas(root, width=100,height=100)
+        self.picture.create_rectangle(0,0,100,100,fill="green")
+        self.picture.place(x=0,y=0)
+        pass
+
 root = tkinter.Tk()
 root.geometry("200x200")
 root.title("Test window")
 
-picture = tkinter.Canvas(root, width=100,height=100)
-picture.create_rectangle(0,0,100,100,fill="green")
-picture.place(x=0,y=0)
+testclass = graphic()
 
 
 root.mainloop()
